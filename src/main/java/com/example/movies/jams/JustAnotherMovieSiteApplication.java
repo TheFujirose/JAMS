e1fd4961b73b26a24d45593906fedba9dbce8ad6
TheFujirose <50027005+TheFujirose@users.noreply.github.com>
2026-02-27 19:52:17 -0500

    Feat: Spring Init

---
+++ src/main/java/com/example/movies/jams/JustAnotherMovieSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.movies.jams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAnotherMovieSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAnotherMovieSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movies/jams/JustAnotherMovieSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movies.jams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAnotherMovieSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
